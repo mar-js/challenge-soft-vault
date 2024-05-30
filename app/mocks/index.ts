3ed8db9e2d2592ff176816160984844ba419f57b
Martin <maguera37@gmail.com>
2024-05-30 20:03:08 -0300

    feat: create mocks folder and add cardsOurVisions mock

---
+++ app/mocks/index.ts
@@ -0,0 +1,22 @@
+import type { ICardOurVisions } from "../ts/interfaces";
+
+export const cardsOurVisions: ICardOurVisions[] = [
+	{
+		id: 1,
+		title: "Understanding needs",
+		description:
+			"We identify who has the problem or need and provide a detailed description of what the problem entails.",
+	},
+	{
+		id: 2,
+		title: "Identifying opportunities",
+		description:
+			"Lorem Ipsum is simply dummy text of the printin and typesetting industry. Lorem Ipsum has been the industry's standard.",
+	},
+	{
+		id: 3,
+		title: "Developing customized solutions",
+		description:
+			"Lorem Ipsum is simply dummy text of the printin and typesetting industry. Lorem Ipsum has been the industry's standard.",
+	},
+];
