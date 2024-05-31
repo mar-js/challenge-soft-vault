1a7ef4ff61314b5ef9f6202c013ad930e08b2882
Martin <maguera37@gmail.com>
2024-05-31 00:34:07 -0300

    feat: change components in home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,17 @@
 "use client";
-import { CardOurVisions, Flex, Main } from "./UI";
-import { cardsOurVisions } from "./mocks";
+import { Card, Flex, Main } from "./UI";
+import { cardsOurVisions, cardsProjects } from "./mocks";
 
 export default function Home() {
 	return (
 		<Main>
 			<Flex>
 				{cardsOurVisions.map((card) => (
-					<CardOurVisions
-						key={card.id}
-						id={card.id}
-						title={card.title}
-						description={card.description}
-						bg={card.bg}
-						padding={card.padding}
-					/>
+					<Card key={card.id} {...card} />
 				))}
+			</Flex>
+			<Flex>
+				<Card {...cardsProjects} />
 			</Flex>
 		</Main>
 	);
