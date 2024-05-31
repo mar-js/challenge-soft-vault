1b025ce7c26c2e5d1c15c643030189040dd75397
Martin <maguera37@gmail.com>
2024-05-30 22:50:53 -0300

    feat: add flex and CardOurVisions components and cardsOurVisions mock

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,22 @@
 "use client";
-
-import { Main } from "./UI";
+import { CardOurVisions, Flex, Main } from "./UI";
+import { cardsOurVisions } from "./mocks";
 
 export default function Home() {
-	return <Main>Helo</Main>;
+	return (
+		<Main>
+			<Flex>
+				{cardsOurVisions.map((card) => (
+					<CardOurVisions
+						key={card.id}
+						id={card.id}
+						title={card.title}
+						description={card.description}
+						bg={card.bg}
+						padding={card.padding}
+					/>
+				))}
+			</Flex>
+		</Main>
+	);
 }
