260a64daa148835ee1e07719c5f6881232868dff
Martin <maguera37@gmail.com>
2024-05-31 09:53:05 -0300

    feat: add data-testid attr to card component

--- app/UI/components/card/index.tsx
+++ app/UI/components/card/index.tsx
@@ -3,7 +3,7 @@ import type { FC } from "react";
 import { CardDescriptionStyle, CardStyle, CardTitleStyle } from "./styles";
 
 export const Card: FC<ICard> = ({ title, description, styles }) => (
-	<CardStyle {...styles}>
+	<CardStyle data-testid="test-card" {...styles}>
 		<CardTitleStyle>{title}</CardTitleStyle>
 		<CardDescriptionStyle>{description}</CardDescriptionStyle>
 	</CardStyle>
