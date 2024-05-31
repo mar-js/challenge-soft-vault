8176302697cabf4aded4ecdf772d1205bafd8ac0
Martin <maguera37@gmail.com>
2024-05-31 13:10:36 -0300

    feat: create fonts file

---
+++ fonts/index.ts
@@ -0,0 +1,4 @@
+import { Sen, Syne } from "next/font/google";
+
+export const syneFont = Syne({ subsets: ["latin"], variable: "--font-syne" });
+export const senFont = Sen({ subsets: ["latin"], variable: "--font-sen" });
