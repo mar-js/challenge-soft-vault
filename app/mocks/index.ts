3a9d4561b39f072d8b2d18289308e750628bf421
Martin <maguera37@gmail.com>
2024-05-30 22:46:46 -0300

    feat: add bg image and padding style to cardsOurVisions in mocks

--- app/mocks/index.ts
+++ app/mocks/index.ts
@@ -1,3 +1,6 @@
+import bgOne from "../assets/images/bg-one.png";
+import bgThree from "../assets/images/bg-three.png";
+import bgTwo from "../assets/images/bg-two.png";
 import type { ICardOurVisions } from "../ts/interfaces";
 
 export const cardsOurVisions: ICardOurVisions[] = [
@@ -6,17 +9,23 @@ export const cardsOurVisions: ICardOurVisions[] = [
 		title: "Understanding needs",
 		description:
 			"We identify who has the problem or need and provide a detailed description of what the problem entails.",
+		bg: bgOne.src,
+		padding: "6rem 5.6rem 3.5rem",
 	},
 	{
 		id: 2,
 		title: "Identifying opportunities",
 		description:
 			"Lorem Ipsum is simply dummy text of the printin and typesetting industry. Lorem Ipsum has been the industry's standard.",
+		bg: bgTwo.src,
+		padding: "6rem 3rem 3.5rem 6rem",
 	},
 	{
 		id: 3,
 		title: "Developing customized solutions",
 		description:
 			"Lorem Ipsum is simply dummy text of the printin and typesetting industry. Lorem Ipsum has been the industry's standard.",
+		bg: bgThree.src,
+		padding: "6rem 3rem 3.5rem 4rem",
 	},
 ];
