9b7420c3d94e742174e65a5c68ffda1baca5ec26
Martin <maguera37@gmail.com>
2024-05-30 22:49:45 -0300

    feat: create ICardOurVisionsStyle and IFlex interfaces

--- app/ts/interfaces/index.ts
+++ app/ts/interfaces/index.ts
@@ -2,4 +2,17 @@ export interface ICardOurVisions {
 	id: number;
 	title: string;
 	description: string;
+	bg: string;
+	padding: string;
+}
+
+export interface ICardOurVisionsStyle {
+	bg: string;
+	padding: string;
+}
+
+export interface IFlex {
+	justifyContent?: string;
+	alignItem?: string;
+	flexWrap?: string;
 }
